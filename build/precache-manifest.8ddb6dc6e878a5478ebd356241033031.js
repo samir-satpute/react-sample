23d599fda53eba070b4ad3d95bca4c6cf1f8d38b
Samir Satpute <samirs@winjit.com>
2020-03-27 17:28:56 +0530

    minor changes

---
+++ build/precache-manifest.8ddb6dc6e878a5478ebd356241033031.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9a849023c0f90f3d2828e260a4930bd2",
+    "url": "/index.html"
+  },
+  {
+    "revision": "e524bda454e4c23f565e",
+    "url": "/static/css/2.de424728.chunk.css"
+  },
+  {
+    "revision": "98b1b2fb08d0566e15b1",
+    "url": "/static/css/main.928846c4.chunk.css"
+  },
+  {
+    "revision": "e524bda454e4c23f565e",
+    "url": "/static/js/2.25072e22.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/static/js/2.25072e22.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "98b1b2fb08d0566e15b1",
+    "url": "/static/js/main.c76c85ba.chunk.js"
+  },
+  {
+    "revision": "59952fdebc910aedde3f",
+    "url": "/static/js/runtime-main.b76a5597.js"
+  },
+  {
+    "revision": "4bfe2f122205eb3f0a2bf69b5a5c2406",
+    "url": "/static/media/banner-img.4bfe2f12.png"
+  },
+  {
+    "revision": "5a791230552896569659546d3de61e90",
+    "url": "/static/media/logo.5a791230.png"
+  },
+  {
+    "revision": "70f16c6f0847a5cf85767caaef3d2317",
+    "url": "/static/media/orange-banner.70f16c6f.png"
+  }
+]);
--- build/service-worker.js
+++ build/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.851e2d086785826a6dd096bb445566af.js"
+  "/precache-manifest.8ddb6dc6e878a5478ebd356241033031.js"
 );
 
 self.addEventListener('message', (event) => {
